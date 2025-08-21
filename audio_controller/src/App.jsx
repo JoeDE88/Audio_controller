2255a9d15d5dd785ab7b21a3e9451da581a3d0f2
Joe Lizardo Diaz Echevarria <joediaz88@hotmail.com>
2025-08-21 19:54:03 +0000

    refactored osc1 functions to one unique changeOsc1 with frequency and detune  settings

--- audio_controller/src/App.jsx
+++ audio_controller/src/App.jsx
@@ -2,47 +2,43 @@ import { useState } from 'react';
 import './App.scss'
 import Osc1 from './components/Osc1';
 
-// se ejecuta una nueva de instancia de AudioContext solo una vez:
+// new AudioContext instance started:
 let actx = new AudioContext();
 
-// declaro la salida (audio) del audio:
+// audio output defined:
 let out = actx.destination;
 
-// se crea un oscillator (frecuencia de onda) y un gain:
+// oscillator and gain created:
 let osc1 = actx.createOscillator();
 let gain1 = actx.createGain();
 
-// se conecta el oscillator con el gain:
+// oscillator connected to gain:
 osc1.connect(gain1);
 
-// se conecta el gain con la salida:
+// gain connected to out:
 gain1.connect(out)
 
-// finalmente se inicializa el audio:
+// starting the oscillator:
 // osc1.start()
 
 function App() {
-  const [osc1Freq, setOsc1Freq] = useState(osc1.frequency.value)
-  const [osc1Detune, setOsc1Detune] = useState(osc1.detune.value)
+  const [osc1Settings, setOsc1Settings] = useState({
+    frequency: osc1.frequency.value ,
+    detune: osc1.detune.value
+  })
 
-  const changeOsc1Freq = (e) =>{
-    let {value} = e.target;
-    setOsc1Freq(value)
-    osc1.frequency.value = value
-  }
-
-  const changeOsc1Detune = (e) =>{
-    let {value} = e.target
-    setOsc1Detune(value)
-    osc1.detune.value = value
-  }
+  function changeOsc1(e){
+    let {value, id} = e.target;
+    setOsc1Settings({...osc1Settings, [id]: value})
+    osc1[id].value = value
+}
 
   return (
     <>
     <h1>Sliders</h1>
     <button onClick={()=> osc1.start()}>Start</button>
     <button onClick={()=> osc1.stop()}>Stop</button>
-    <Osc1 changeFreq={changeOsc1Freq} freq={osc1Freq} changeDetune={changeOsc1Detune} detune={osc1Detune}/>
+    <Osc1 changeSettings={changeOsc1} settings={osc1Settings}/>
     </>
   )
 }
--- audio_controller/src/components/Osc1.jsx
+++ audio_controller/src/components/Osc1.jsx
@@ -1,14 +1,14 @@
-export default function Osc1({changeFreq, freq,changeDetune, detune}){
+export default function Osc1({changeSettings, settings}){
   return (
     <div className="control">
         <h1>Osc 1</h1>
         <div className="param">
             <h3>frequency</h3>
-        <input value={freq} onChange={changeFreq} type="range" id="frequency" max='5000'/>
+        <input value={settings.frequency} onChange={changeSettings} type="range" id="frequency" max='5000'/>
         </div>
         <div className="param">
             <h3>detune</h3>
-        <input value={detune} onChange={changeDetune} type="range" id="detune" max='100'/>
+        <input value={settings.detune} onChange={changeSettings} type="range" id="detune" max='100'/>
         </div>
     </div>
   )
